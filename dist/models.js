edf08dbcfdaef15797e191c1ba1e6a6e11bf11ea
Luke Geiger <lukejamesgeiger@gmail.com>
2024-02-27 09:00:24 -0500

    added models

---
+++ dist/models.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.weights = void 0;
+exports.weights = {
+    view: 1,
+    like: 2,
+    share: 3,
+    comment: 4,
+    save: 5,
+    follow: 6,
+    purchase: 7,
+};
+//# sourceMappingURL=models.js.map
