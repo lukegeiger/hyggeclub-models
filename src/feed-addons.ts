b30aaa8a349969d01d9b9c6daf192c19067f273a
Luke Geiger <lukejamesgeiger@gmail.com>
2024-03-07 21:59:37 -0500

    arch

---
+++ src/feed-addons.ts
@@ -0,0 +1,20 @@
+import { ContentItem, ArticleContentItem, UserFeed, } from "./models";
+
+export function isArticleContentItem(contentItem: ContentItem): contentItem is ArticleContentItem {
+    return contentItem.media_type === 'article';
+}
+  
+export function getContentItemsByType<T extends ContentItem>(
+    contentItems: ContentItem[],
+    checkType: (contentItem: ContentItem) => contentItem is T
+  ): T[] {
+    return contentItems.filter(checkType);
+  }
+  
+
+  function getAllArticlesFromFeed(feed: UserFeed): ArticleContentItem[] {
+    return feed.sections
+      .flatMap(section => section.contentItems) // Flatten all content items from all sections
+      .filter(isArticleContentItem); // Filter out only articles using the type guard
+  }
+  
