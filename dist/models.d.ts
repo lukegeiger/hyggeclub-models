9f53051143aee5ca36e99bb856b7b00c20c37a3f
Luke Geiger <lukejamesgeiger@gmail.com>
2024-03-08 09:57:39 -0500

    id

--- dist/models.d.ts
+++ dist/models.d.ts
@@ -104,6 +104,7 @@ export interface RedisContentItem {
  * Models a section of the user feed, grouping content items by theme or category.
  */
 export interface FeedSection {
+    id: string;
     title: string | null;
     contentItems: ContentItem[];
 }
